b00f52fd2980a8cee30a2c0b014f7baa4446db98
olsons <solson3@nd.edu>
2021-08-09 11:52:57 -0400

    updated hovering

--- script.js
+++ script.js
@@ -303,16 +303,3 @@ function sentenceCase(str) {
 	}
 	return str;
 }
-
-function showTooltip(evt, text) {
-  let tooltip = document.getElementById("tooltip");
-  tooltip.innerHTML = text;
-  tooltip.style.display = "block";
-  tooltip.style.left = evt.pageX + 10 + 'px';
-  tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function hideTooltip() {
-  var tooltip = document.getElementById("tooltip");
-  tooltip.style.display = "none";
-}
